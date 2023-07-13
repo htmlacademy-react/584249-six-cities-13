969ff0c296dca318ed8fe77e74d5564f1de03b3b
AigerimYerk <aigerim@lessons.kz>
2023-07-13 11:15:31 +0600

    Insert deleted tabindex, use Array to add OfferCard 5 times

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -81,26 +81,23 @@ function MainPage({offersNumber}: MainPageProps): JSX.Element {
               <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type">
+                <span className="places__sorting-type" tabIndex={0}>
                   Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
                     <use xlinkHref="#icon-arrow-select" />
                   </svg>
                 </span>
                 <ul className="places__options places__options--custom">
-                  <li className="places__option places__option--active">Popular</li>
-                  <li className="places__option">Price: low to high</li>
-                  <li className="places__option">Price: high to low</li>
-                  <li className="places__option">Top rated first</li>
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                <span>{offersNumber}</span>
-                <OfferCard />
-                <OfferCard />
-                <OfferCard />
-                <OfferCard />
-                <OfferCard />
+                {Array.from({ length: offersNumber }, (_, index) => (
+                  <OfferCard key={index} />
+                ))}
               </div>
             </section>
             <div className="cities__right-section">
