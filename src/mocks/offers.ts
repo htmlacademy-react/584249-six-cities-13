be9d453600b924968e0c6966ca0759bb653ded31
AigerimYerk <aigerim@lessons.kz>
2023-07-26 16:56:01 +0600

    Created mock file, and types for data

---
+++ src/mocks/offers.ts
@@ -0,0 +1,169 @@
+import { TypeOfferPage } from '../types/offer';
+
+export const Offers: TypeOfferPage[] = [
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+    title: 'Beautiful & luxurious apartment at great location in Amsterdam',
+    type: 'apartment',
+    price: 120,
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 4,
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    bedrooms: 3,
+    goods: [
+      'Heating',
+      'Wi-Fi',
+      'Kitchen',
+      'Fridge',
+      'Washing machine',
+      'Coffee machine',
+      'Dishwasher',
+      'Towels',
+      'Baby seat',
+      'Cabel TV'
+    ],
+    host: {
+      name: 'Angelina Conner',
+      avatarUrl: 'img/avatar-angelina.jpg',
+      isPro: true
+    },
+    images: [
+      'img/rooms.jpg',
+      'img/apartment-01.jpg',
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+      'img/studio-02.jpg',
+    ],
+    maxAdults: 4
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f01',
+    title: 'Luxurious studio at great location',
+    type: 'studio',
+    price: 80,
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 6
+      }
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 3,
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Paris.',
+    bedrooms: 1,
+    goods: [
+      'Heating',
+      'Wi-Fi',
+      'Kitchen',
+      'Fridge',
+      'Coffee machine',
+      'Towels',
+      'Cabel TV'
+    ],
+    host: {
+      name: 'Oliver Conner',
+      avatarUrl: 'img/avatar-max.jpg',
+      isPro: false
+    },
+    images: [
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+      'img/studio-02.jpg',
+    ],
+    maxAdults: 2
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f02',
+    title: 'Cute private room for one person',
+    type: 'private room',
+    price: 40,
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2,
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    bedrooms: 1,
+    goods: [
+      'Heating',
+      'Wi-Fi',
+      'Towels',
+    ],
+    host: {
+      name: 'Oliver Conner',
+      avatarUrl: 'img/avatar-max.jpg',
+      isPro: true
+    },
+    images: [
+      'img/apartment-02.jpg',
+      'img/rooms.jpg',
+      'img/studio-02.jpg',
+    ],
+    maxAdults: 1
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f03',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    price: 180,
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    isFavorite: true,
+    isPremium: true,
+    rating: 5,
+    description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    bedrooms: 4,
+    goods: [
+      'Heating',
+      'Wi-Fi',
+      'Kitchen',
+      'Fridge',
+      'Washing machine',
+      'Coffee machine',
+      'Dishwasher',
+      'Towels',
+      'Baby seat',
+      'Cabel TV',
+      'Parking',
+      'Room service'
+    ],
+    host: {
+      name: 'Angelina Conner',
+      avatarUrl: 'img/avatar-angelina.jpg',
+      isPro: true
+    },
+    images: [
+      'img/rooms.jpg',
+      'img/apartment-01.jpg',
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+      'img/studio-02.jpg',
+      'img/rooms.jpg',
+    ],
+    maxAdults: 6
+  }
+];
