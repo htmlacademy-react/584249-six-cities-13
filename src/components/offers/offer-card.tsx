cfed0d33338f96890a36ebc2635d8d04819c0c90
=AigerimY <=yy.aigerim@gmail.com>
2023-08-07 10:35:43 +0600

    Fixed offer-card component, used data

--- src/components/offers/offer-card.tsx
+++ src/components/offers/offer-card.tsx
@@ -1,37 +1,44 @@
-function OfferCard(): JSX.Element {
+import { TypeOfferPage } from '../../types/offer';
+
+type OfferCardProps = {
+  oneOffer: TypeOfferPage;
+}
+
+function OfferCard({oneOffer}: OfferCardProps): JSX.Element {
   return (
     <article className="cities__card place-card">
+      {oneOffer.isPremium &&
       <div className="place-card__mark">
         <span>Premium</span>
-      </div>
+      </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={oneOffer.images[0]} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro; {oneOffer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
-              {/* <use xlink:href="#icon-bookmark"></use> */}
+              <use xlinkHref="#icon-bookmark" />
             </svg>
             <span className="visually-hidden">To bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            {/* <span style="width: 80%"></span> */}
+            <span style={{ width: String(oneOffer.rating * 20).concat('%')}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{oneOffer.title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{oneOffer.type}</p>
       </div>
     </article>
   );
