3d02ffa430fb2990c6584bdd9e6e514a52c4f358
=AigerimY <=yy.aigerim@gmail.com>
2023-08-16 13:30:15 +0600

    Created map

---
+++ src/components/map/map.tsx
@@ -0,0 +1,54 @@
+import { Icon, Marker } from 'leaflet';
+import { useEffect, useRef } from 'react';
+import useMap from '../../hooks/map/map';
+import { Location, TypeOfferPage } from '../../types/offer';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  className: string;
+  city: Location;
+  offers: TypeOfferPage[];
+  selectedOfferId?: string | null;
+}
+
+const defaultCustomIcon = new Icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: './img/pin-active.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39]
+});
+
+function Map({ className, city, offers, selectedOfferId }: MapProps): JSX.Element {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        const marker = new Marker({
+          lat: offer.city.location.latitude,
+          lng: offer.city.location.longitude,
+        });
+
+        marker
+          .setIcon(
+            selectedOfferId && offer.id === String(selectedOfferId)
+              ? currentCustomIcon
+              : defaultCustomIcon
+          )
+          .addTo(map);
+      });
+    }
+  }, [map, offers, selectedOfferId]);
+
+  return (
+    <section className={`${className}__map map`} ref={mapRef}></section>
+  );
+}
+
+export default Map;
