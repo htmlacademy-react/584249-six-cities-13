02ad28f5d246b43d3d784106fdfdf5785abb19a1
=AigerimY <=yy.aigerim@gmail.com>
2023-08-21 14:40:39 +0600

    Created sort component

---
+++ src/components/sort/sort.tsx
@@ -0,0 +1,63 @@
+import cn from 'classnames';
+
+import { SortOptions } from '../../const';
+import { useState, useRef, useEffect } from 'react';
+
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { changeSort } from '../../store/action';
+
+function Sort(): JSX.Element {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLUListElement>(null);
+
+  const dispatch = useAppDispatch();
+  const currentSortOption = useAppSelector((state) => state.sortOption);
+
+  const handleSortClick = (option: string) => {
+    dispatch(changeSort(option));
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setOpen(!open)}>
+        {currentSortOption}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul
+        className={cn('places__options', 'places__options--custom', { 'places__options--opened' : open })}
+        ref={dropdownRef}
+      >
+        {Object.values(SortOptions).map((option) => (
+          <li
+            key={option}
+            className={cn('places__option', {'places__option--active' : option === currentSortOption})}
+            tabIndex={0}
+            onClick={() => handleSortClick(option)}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+}
+
+export default Sort;
