77aabb71c45f91f8d54fc53492d31ccbe1ca420e
=AigerimY <=yy.aigerim@gmail.com>
2023-08-21 14:38:37 +0600

    Added props and actions to select offer and change sort option

--- src/store/action.ts
+++ src/store/action.ts
@@ -3,3 +3,11 @@ import { createAction } from '@reduxjs/toolkit';
 export const cityChange = createAction('offers/cityChange', (city: string) => ({
   payload: city,
 }));
+
+export const selectOffer = createAction('offer/selectOffer', (id: string) => ({
+  payload: id,
+}));
+
+export const changeSort = createAction('offers/changeSort', (sortOption: string) => ({
+  payload: sortOption,
+}));
