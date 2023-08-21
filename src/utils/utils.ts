fbedbf2d3c7ee64443e14b6d505d05cb3bab22fb
=AigerimY <=yy.aigerim@gmail.com>
2023-08-21 14:40:23 +0600

    Created urils for functions, added function for offers sorting

---
+++ src/utils/utils.ts
@@ -0,0 +1,15 @@
+import { TypeOfferPage } from '../types/offer';
+
+export function sortOffers(offers: TypeOfferPage[], sortBy: string): TypeOfferPage[] {
+  switch (sortBy) {
+    case 'Price: high to low':
+      return offers.slice().sort((a, b) => b.price - a.price);
+    case 'Price: low to high':
+      return offers.slice().sort((a, b) => a.price - b.price);
+    case 'Top rated first':
+      return offers.slice().sort((a, b) => b.rating - a.rating);
+    default:
+      return offers;
+  }
+}
+
