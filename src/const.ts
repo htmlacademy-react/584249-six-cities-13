728f69d0565c3c8e6de6bb542736628437db6266
=AigerimY <=yy.aigerim@gmail.com>
2023-08-22 15:43:10 +0600

    Using axios

--- src/const.ts
+++ src/const.ts
@@ -1,21 +1,21 @@
-export const Settings = {
+const Settings = {
   OffersCount: 5
 };
 
-export enum AppRoutes {
+enum AppRoutes {
   Login = '/login',
   Favorites = '/favorites',
   Offer = '/offer/:id',
   Root = '/'
 }
 
-export enum AuthorizationStatus {
+enum AuthorizationStatus {
   Auth = 'AUTH',
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
 
-export const CITIES = [
+const CITIES = [
   'Paris',
   'Cologne',
   'Brussels',
@@ -24,9 +24,20 @@ export const CITIES = [
   'Dusseldorf',
 ];
 
-export const SortOptions = {
+const SortOptions = {
   POPULAR: 'Popular',
   PRICE_LOW_TO_HIGHT: 'Price: low to high',
   PRICE_HIGHT_TO_LOW: 'Price: high to low',
   TOP_RATED_FIRST: 'Top rated first'
 };
+
+enum APIRoute {
+  Offers = '/hotels',
+  Comments = '/comments',
+  Login = '/login',
+  Logout = '/logout'
+}
+
+const TIMEOUT_SHOW_ERROR = 2000;
+
+export { Settings, AppRoutes, AuthorizationStatus, SortOptions, APIRoute, TIMEOUT_SHOW_ERROR, CITIES };
