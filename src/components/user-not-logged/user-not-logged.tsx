389094a005f77c72d8d662aef3a9dbd39dc62632
=AigerimY <=yy.aigerim@gmail.com>
2023-08-23 13:55:16 +0600

    Form components

---
+++ src/components/user-not-logged/user-not-logged.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+
+export default function UserNotLogged(): JSX.Element {
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        <li className="header__nav-item user">
+          <Link
+            className="header__nav-link header__nav-link--profile"
+            to={AppRoutes.Login}
+          >
+            <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+            <span className="header__login">Sign in</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
