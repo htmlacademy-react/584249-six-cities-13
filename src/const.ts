c309509de27c7e3806efb8c2c6a3a3165ba58bb7
=AigerimY <=yy.aigerim@gmail.com>
2023-08-28 08:58:19 +0600

    Fixed data

--- src/const.ts
+++ src/const.ts
@@ -13,31 +13,48 @@ enum AuthorizationStatus {
   Auth = 'AUTH',
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
+  Loading = 'LOADING'
 }
 
-const CITIES = [
-  'Paris',
-  'Cologne',
-  'Brussels',
-  'Amsterdam',
-  'Hamburg',
-  'Dusseldorf',
-];
+enum Cities {
+  Paris = 'Paris',
+  Cologne = 'Cologne',
+  Brussels = 'Brussels',
+  Amsterdam = 'Amsterdam',
+  Hamburg = 'Hamburg',
+  Dusseldorf = 'Dusseldorf'
+}
 
-const SortOptions = {
-  POPULAR: 'Popular',
-  PRICE_LOW_TO_HIGHT: 'Price: low to high',
-  PRICE_HIGHT_TO_LOW: 'Price: high to low',
-  TOP_RATED_FIRST: 'Top rated first'
-};
+enum SortOptions {
+  Popular = 'Popular',
+  LowToHigh = 'Price: low to high',
+  HighToLow = 'Price: high to low',
+  TopRatedFirst = 'Top rated first',
+}
 
 enum APIRoute {
+  Nearby = '/offers',
   Offers = '/hotels',
-  Comments = '/comments',
+  Reviews = '/comments',
   Login = '/login',
   Logout = '/logout'
 }
 
+enum NameSpace {
+  Offers = 'OFFERS',
+  User = 'USER',
+  App = 'APP',
+  Offer = 'OFFER',
+  Reviews = 'REVIEWS'
+}
+
+enum FetchStatus {
+  Idle = 'Idle',
+  Loading = 'Loading',
+  Success = 'Success',
+  Failed = 'Failed',
+}
+
 const TIMEOUT_SHOW_ERROR = 2000;
 
-export { Settings, AppRoutes, AuthorizationStatus, SortOptions, APIRoute, TIMEOUT_SHOW_ERROR, CITIES };
+export { FetchStatus, NameSpace, Settings, AppRoutes, AuthorizationStatus, SortOptions, APIRoute, TIMEOUT_SHOW_ERROR, Cities };
